c7f87bee55ee384a842d5b2e72d0719e7822d263
Paul Bottein <paul.bottein@gmail.com>
2020-01-19 21:37:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# climate-mode-entity-row
+Show multiple entity states or attributes on entity rows in Home Assistant's Lovelace UI
+
+## Setup
+
+Manually add [climate-mode-entity-row.js](https://raw.githubusercontent.com/piitaya/lovelace-climate-mode-entity-row/master/climate-mode-entity-row.js)
+to your `<config>/www/` folder and add the following to your `ui-lovelace.yaml` file:
+```yaml
+resources:
+  - url: /local/climate-mode-entity-row.js?v=1.0.0
+    type: module
+```
+
+OR install using [HACS](https://hacs.xyz/) and add this instead:
+```yaml
+resources:
+  - url: /community_plugin/lovelace-climate-mode-entity-row/climate-mode-entity-row.js
+    type: module
+```
+
+## Options
+
+| Name | Type | Default | Description
+| ---- | ---- | ------- | -----------
+| type | string | **Required** | `custom:climate-mode-entity-row`
+| entity | string | **Required** | `domain.my_entity_id`
+| name | string | | Override entity `friendly_name`
+| unit | string | | Override entity `unit_of_measurement`
+| icon | string | | Override entity `icon`
+| | | |
+| modes | object | *see below* | Modes
+
+### Mode objects
+
+| Name | Type | Default | Description
+| ---- | ---- | ------- | -----------
+| icon | string | **Required** | A icon for the mode
+| hvac_mode | string | | A valid hvac_mode for the entity
+| preset_mode | string | | A valid preset_mode for the entity
+| color | string | | Custom color for the icon when toggle
+
+## Example
+
+```yaml
+type: entities
+  - entity: climate.thermostat_kitchen
+    type: 'custom:thermostat-card'
+    modes:
+      - icon: 'mdi:snowflake'
+        hvac_mode: 'off'
+        color: '#B3E5FC'
+      - icon: 'mdi:moon-waxing-crescent'
+        preset_mode: eco
+        color: '#9575CD'
+      - icon: 'mdi:weather-sunny'
+        preset_mode: comfort
+        color: '#FFC107'
+
+```
---
+++ climate-mode-entity-row.js
@@ -0,0 +1,109 @@
+(LitElement => {
+  const html = LitElement.prototype.html;
+  const css = LitElement.prototype.css;
+
+  class ClimateModeEntity extends LitElement {
+    static get properties() {
+      return {
+        _hass: {},
+        _config: {},
+        state: {}
+      };
+    }
+
+    static get styles() {
+      return css`
+        .flex {
+          display: flex;
+          align-items: center;
+          flex-wrap: nowrap;
+          justify-content: space-between;
+          align-items: center;
+        }
+        ha-icon {
+          cursor: pointer;
+          color: var(--secondary-text-color);
+        }
+        ha-icon:not(:last-child) {
+          margin-right: 8px;
+        }
+        ha-icon.active {
+          color: var(--primary-color);
+        }
+      `;
+    }
+
+    render() {
+      const config = this._config;
+      const hass = this._hass;
+
+      return html`
+        <hui-generic-entity-row .hass="${hass}" .config="${config}">
+          <div class="flex">
+            ${config.modes.map(mode => this.renderMode(mode))}
+          </div>
+        </hui-generic-entity-row>
+      `;
+    }
+
+    renderMode(mode) {
+      const isActive =
+        ((!mode.preset_mode || mode.preset_mode === this.state.preset_mode) &&
+          !mode.hvac_mode) ||
+        mode.hvac_mode === this.state.hvac_mode;
+
+      const onClick = () => this.setMode(mode);
+
+      const style = mode.color ? `color: ${mode.color}` : "";
+    
+      return html`
+        <ha-icon
+          style="${isActive ? style : ""}"
+          class="${isActive ? "active" : ""}"
+          icon="${mode.icon}"
+          @click="${onClick}"
+        ></ha-icon>
+      `;
+    }
+
+    setMode(mode) {
+      if (mode.hvac_mode) {
+        this._hass.callService("climate", "set_hvac_mode", {
+          entity_id: this._config.entity,
+          hvac_mode: mode.hvac_mode
+        });
+      }
+
+      if (mode.preset_mode) {
+        this._hass.callService("climate", "set_preset_mode", {
+          entity_id: this._config.entity,
+          preset_mode: mode.preset_mode
+        });
+      }
+    }
+
+    setConfig(config) {
+      if (!config.entity) throw new Error("Please define a entity.");
+      if (config.entity.split(".")[0] !== "climate")
+        throw new Error("Please define a climate entity.");
+      this._config = config;
+    }
+
+    set hass(hass) {
+      this._hass = hass;
+
+      const entity = hass.states[this._config.entity];
+
+      const hvac_mode = entity.state;
+      const preset_mode = entity.attributes.preset_mode;
+
+      this.state = {
+        entity,
+        hvac_mode,
+        preset_mode
+      };
+    }
+  }
+
+  customElements.define("climate-mode-entity-row", ClimateModeEntity);
+})(window.LitElement || Object.getPrototypeOf(customElements.get("hui-view")));
