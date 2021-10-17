ae1e5519e99d04b43fdb175aaba500bf10ae6eeb
Daniel Wiege <d.wiege@googlemail.com>
2021-10-17 21:34:18 +0200

    Revert "Updated css"
    This reverts commit a61abd87e0a7cd245dc93f3f01e1af0d0a5b4981.

--- climate-mode-entity-row.js
+++ climate-mode-entity-row.js
@@ -39,7 +39,7 @@
           align-items: center;
         }
         .state {
-          min-width: 65px;
+          min-width: 45px;
           text-align: end;
           margin-left: 5px;
         }
